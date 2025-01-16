17398482fc454ffe0c5d68100e4e02ec6f66561c
Oliver <oliver.efthemes@gmail.com>
2025-01-16 14:51:13 -0800

    Refactor Game class and add endGame() method

--- src/main/java/edu/bothell/multi_ui/core/Game.java
+++ src/main/java/edu/bothell/multi_ui/core/Game.java
@@ -38,22 +38,51 @@ public class Game {
     
     public boolean isValid(int[] pos, String sId){
         System.out.println("isVAlid?"+s.getIt(pos)+"|" + sId+"|" + active.getSId()+"|");
+
+        if(pos[0] > 2 || pos[1] > 2) return false;
+
         return s.isOpen(pos) && active.getSId().equals(sId);
+    }
+    public boolean checkGameOver(){
+
+        
+        // check draws
+        if(turn > 8) return true;
+        
+        // check rows
+        for(int y = 0; y < 3; y++){
+            if(s.getIt(0, y) == s.getIt(1, y) && s.getIt(1, y) == s.getIt(2, y)) return true;
+        }
+        // check columns
+        for(int x = 0; x < 3; x++){
+            if(s.getIt(x, 0) == s.getIt(x, 1) && s.getIt(x, 1) == s.getIt(x, 2)) return true;
+        }
+        // check diagonals
+        if(s.getIt(0, 0) == s.getIt(1, 1) && s.getIt(1, 1) == s.getIt(2, 2)) return true;
+        // check diagonals other way
+        if(s.getIt(0, 2) == s.getIt(1, 1) && s.getIt(1, 1) == s.getIt(2, 0)) return true;
+
+        
+        
+        return false;
     }
 
     public char play(int[] pos, String sId){
         if(!isValid(pos, sId)) return ' ';
         turn++;
         this.s.setIt(active.getChar(), pos[0], pos[1]);
+
+        if(checkGameOver()) return active.getChar();
+
+
+        
         this.active = p.get( turn % p.size() );
 
         return active.getChar();
     }
 
 
-    public void endGame(){
-        //TODO
-    }
+
     public Player getActive() {
         return this.active;
     }
